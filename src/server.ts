91a9971b66a5047cd6dfedc80e16d6ff6711e947
kleves Henrique <kleveshenrique@gmail.com>
2023-03-19 23:02:35 -0300

    updated address origin request

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 // }
 
 app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin","http://localhost:3000")
+    res.header("Access-Control-Allow-Origin","https://food-comerce.netlify.app")
     res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE')
     app.use(cors())
     next()
